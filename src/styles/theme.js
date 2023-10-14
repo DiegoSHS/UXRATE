94c966b281248f8dda95715535cb43b3909cb369
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2023-10-13 20:05:43 -0600

    changes in cookie provider, setcookie created
    Signed-off-by: Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,25 +1,32 @@
 import { TopBarScroll } from '@/components/topBar'
 import { Context } from '@/context/context'
 import { Container, CssBaseline, ThemeProvider, createTheme } from '@mui/material'
+import { CookiesProvider, useCookies } from 'react-cookie'
 import { Toaster } from 'react-hot-toast'
 
 const defaultTheme = createTheme()
 const darkTheme = createTheme({
     palette: { mode: 'dark' }
 })
-const themes = { darkTheme, defaultTheme }
 
 export default function Layout({ children }) {
+    const themes = [darkTheme, defaultTheme]
+    const [cookies] = useCookies(['theme'])
+    const theme = cookies.theme ? 0 : 1
     return (
-        <ThemeProvider theme={defaultTheme}>
-            <CssBaseline enableColorScheme />
-            <Toaster />
-            <Context>
-                <Container sx={{ my: 10, display: 'flex', flexDirection: 'column', alignItems: 'center' }} fixed component='main' maxWidth='sm'>
-                    <TopBarScroll />
-                    {children}
-                </Container>
-            </Context>
-        </ThemeProvider>
+        <Context>
+            <CookiesProvider>
+                <ThemeProvider theme={themes[theme]}>
+                    <CssBaseline enableColorScheme />
+                    <Toaster />
+                    <Container sx={{ my: 10, display: 'flex', flexDirection: 'column', alignItems: 'center' }} fixed component='main' maxWidth='sm'>
+                        <title>UXRATE</title>
+                        <TopBarScroll />
+                        {children}
+                    </Container>
+                </ThemeProvider>
+            </CookiesProvider>
+        </Context>
+
     )
 }
