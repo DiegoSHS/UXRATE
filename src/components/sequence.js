f796ef1dee6d219c515062dab1340b5c33d162bd
Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>
2023-10-14 15:18:52 -0600

    added chart to results
    Signed-off-by: Diego López Ariza <52292080+DiegoSHS@users.noreply.github.com>

--- src/components/sequence.js
+++ src/components/sequence.js
@@ -1,62 +1,11 @@
-import { Box, Button, CircularProgress, Paper, Stepper } from '@mui/material'
+import { Box, Button, Paper, Stepper } from '@mui/material'
 import { useState } from 'react'
 import { RateResults } from './resume'
 import { RateStep } from './steps'
 import { StoredContext } from '@/context/context'
 import { saveRecord } from '@/requests/uxrecord'
 import toast from 'react-hot-toast'
-
-const steps = [
-    {
-        label: 'Ley de Prägnanz',
-        description: `La simplicidad facilita la vida de los usuarios y mejora su experiencia.`,
-    },
-    {
-        label: 'Ley de Hick',
-        description:
-            'Se debe reducir la carga cognitiva al presentar información de manera clara y limitar las opciones para facilitar la toma de decisiones del usuario.',
-    },
-    {
-        label: 'Ley de Tesler',
-        description: `En el diseño de UX, se debe considerar cómo la simplificación de una característica afecta otras áreas del producto para lograr un equilibrio entre la simplicidad y la funcionalidad.`,
-    },
-    {
-        label: 'Ley de proximidad',
-        description: `Cuando colocamos elementos visuales cercanos entre sí, los usuarios perciben que están relacionados de alguna manera, lo que facilita la organización y la comprensión de la información en la interfaz.`,
-    },
-    {
-        label: 'Efecto Posición Serial',
-        description: `Los elementos clave o críticos deberían ubicarse al principio o al final de una página o lista para que los usuarios los recuerden con mayor facilidad.`,
-    },
-    {
-        label: 'Ley de Fitts',
-        description: `Los elementos de interfaz más grandes y cercanos son más fáciles de alcanzar y seleccionar.`,
-    },
-    {
-        label: 'Ley de Parkinson',
-        description: `Establecer límites de tiempo o proporcionar indicaciones claras de tiempo puede motivar a los usuarios a ser más eficientes y productivos.`,
-    },
-    {
-        label: 'Efecto Von Restorff',
-        description: `Elementos visualmente distintivos, como colores o formas inusuales, pueden ayudar a resaltar información importante, llamando la atención del usuario y mejorando la experiencia al hacer que la información clave sea más memorable y fácil de recordar.`,
-    },
-    {
-        label: 'Principio de Pareto',
-        description: `Concentra tus esfuerzos en lo que realmente importa para los usuarios, ya que la mayoría de los beneficios provienen de un conjunto limitado de aspectos del diseño.`,
-    },
-    {
-        label: 'Efecto Zeigarnik',
-        description: `Esto significa que las interfaces deben proporcionar retroalimentación y pistas visuales para mantener a los usuarios comprometidos y motivados para completar tareas o acciones. Se aplica al mantener a los usuarios interesados y enfocados en un proceso, como un formulario de registro, hasta que se complete, evitando distracciones o abandonos.`,
-    }, ,
-    {
-        label: 'Ley de Miller',
-        description: `Las interfaces no deben abrumar a los usuarios con demasiada información u opciones, ya que podrían tener dificultades para procesarla.`,
-    },
-    {
-        label: 'Ley de Jakob',
-        description: `Las interfaces y los productos deben ser simples y fáciles de usar, evitando la complejidad innecesaria.`,
-    }
-]
+import { steps } from '@/utils/steps'
 
 export default function RateSequence() {
     const { interacts: { name, results }, setInteract, records, setRecords, push } = StoredContext()
@@ -64,16 +13,22 @@ export default function RateSequence() {
     const [sliderValue, setSliderValue] = useState(0)
     const handleSlider = (e, newValue) => {
         setSliderValue(newValue)
+        handleRegister()
     }
     const handleRegister = () => {
-        setRecords([...records, { activeStep, sliderValue }])
+        setRecords((records) => {
+            if (records.length === 0) {
+                return [{ activeStep, sliderValue }]
+            }
+            return [...records.filter(e => e.activeStep !== activeStep), { activeStep, sliderValue }]
+        })
     }
     const deleteRegister = () => {
         setRecords([...records.filter(e => e.activeStep !== activeStep - 1)])
     }
     const handleNext = () => {
-        setActiveStep(activeStep + 1)
         handleRegister()
+        setActiveStep(activeStep + 1)
         setSliderValue(0)
     }
     const handleBack = () => {
@@ -85,6 +40,7 @@ export default function RateSequence() {
         setActiveStep(0)
         setRecords([])
         setInteract({ name: null })
+        setSliderValue(0)
         push('/')
     }
 
