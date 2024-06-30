42a11abf41dc8916f5436de48597ae64cf009126
Diego López Ariza <diegoawesome2017@outlook.com>
2024-06-29 19:31:37 -0600

    Update site validation
    - Added onPaste event
    - Fixed paste behavior

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -4,7 +4,16 @@ export const urlValidator = (urlString) => {
         const ishttps = urlString.substring(0, 8)
         const ishttp = urlString.substring(0, 7)
         url = new URL(urlString)
-        if (ishttps !== 'https://' && ishttp !== 'http://') {
+        const canParse = URL.canParse(urlString)
+        if (
+            (
+                ishttps === 'https://'
+                || ishttp === 'http://'
+            )
+            && canParse
+        ) {
+            err = false
+        } else {
             err = true
         }
     } catch (error) {
